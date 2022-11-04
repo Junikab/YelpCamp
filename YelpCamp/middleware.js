188a86fb3d927f488bb9f35b2118689da3dd472b
Jenny <junikab@gmail.com>
2022-11-04 14:06:27 +1100

    Reviews authorization added

--- YelpCamp/middleware.js
+++ YelpCamp/middleware.js
@@ -1,6 +1,8 @@
 const { campgroundSchema, reviewSchema } = require("./schemas.js");
 const ExpressError = require("./utils/ExpressError");
 const Campground = require("./models/campground");
+const Review = require("./models/review");
+
 
 
 
@@ -34,7 +36,15 @@ module.exports.isAuthor = async (req, res, next) => {
     next();
 };
 
-
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review.author.equals(req.user._id)) {
+        req.flash("error", "You do not have permission to do that!");
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+};
 
 module.exports.validateReview = (req, res, next) => {
     const { error } = reviewSchema.validate(req.body);
--- YelpCamp/routes/reviews.js
+++ YelpCamp/routes/reviews.js
@@ -7,7 +7,7 @@ const ExpressError = require("../utils/ExpressError");
 const Campground = require("../models/campground");
 const Review = require("../models/review");
 const { reviewSchema } = require("../schemas.js");
-const { validateReview } = require("../middleware");
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
 
 
 // Middleware validation *************************************
@@ -15,10 +15,11 @@ const { validateReview } = require("../middleware");
 
 router.post(
     "/",
-    validateReview,
+    validateReview,isLoggedIn,
     catchAsync(async (req, res) => {
         const campground = await Campground.findById(req.params.id);
         const review = new Review(req.body.review);
+        review.author = req.user._id;
         campground.reviews.push(review);
         await review.save();
         await campground.save();
@@ -28,7 +29,7 @@ router.post(
 );
 
 router.delete(
-    "/:reviewId",
+    "/:reviewId", isLoggedIn, isReviewAuthor,
     catchAsync(async (req, res) => {
         const { id, reviewId } = req.params;
         await Campground.findByIdAndUpdate(id, {
