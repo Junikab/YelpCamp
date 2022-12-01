e821175427bb3151c506c40af3eaaf3c5d40d565
Jenny <junikab@gmail.com>
2022-12-01 11:28:36 +1100

    Deleting images from mongo & cloudinary

--- YelpCamp/controllers/campgrounds.js
+++ YelpCamp/controllers/campgrounds.js
@@ -1,4 +1,5 @@
 const Campground = require("../models/campground");
+const {cloudinary} = require("../cloudinary")
 
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -43,6 +44,7 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
+    console.log(req.body)
     const campground = await Campground.findByIdAndUpdate(id, {
         ...req.body.campground,
     });
@@ -51,7 +53,15 @@ module.exports.updateCampground = async (req, res) => {
         filename: f.filename,
     }));
     campground.images.push(...images);
+    if(req.body.deleteImages){
+        for(let filename of req.body.deleteImages){
+            await cloudinary.uploader.destroy(filename)
+        }
+        await campground.updateOne({$pull: {images: {filename: {$in: req.body.deleteImages}}}})
+        console.log(campground)
+    }
     await campground.save()
+    req.flash("success","Campground updated!")
     res.redirect(`/campgrounds/${campground._id}`);
 };
 
