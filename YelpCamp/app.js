5aecf113dc6133114113c67ef91fba27fe0f6975
Jenny <junikab@gmail.com>
2023-02-03 15:20:05 +1100

    change db url to cloud

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -21,7 +21,7 @@ const mongoSanitize = require("express-mongo-sanitize");
 
 const MongoStore = require("connect-mongo");
 
-const dbUrl = "mongodb://localhost:27017/yelp-camp";
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
 
 mongoose.connect(dbUrl, {
     // useNewUrlParser: true,
@@ -51,12 +51,6 @@ app.use(mongoSanitize({replaceWith: "_"}));
 // ******************************************
 const secret = process.env.SECRET || "keyboard_cat";
 
-// const store = new MongoDBStore({
-//     url: dbUrl,
-//     secret,
-//     touchAfter: 24 * 60 * 60,
-// });
-
 const store = MongoStore.create({
     mongoUrl: dbUrl,
     touchAfter: 24 * 60 * 60,
@@ -71,7 +65,7 @@ store.on("error", function(e){
 
 const sessionConfig = {
     store,
-    name: "YelpCamp",
+    name: "session",
     secret,
     resave: false,
     saveUninitialized: true,
